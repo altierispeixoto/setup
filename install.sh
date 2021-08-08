1cdf3c366acca0aff69158187dcfc6d2384f7494
Altiéris (Alt +R) <altierispeixoto@users.noreply.github.com>
2021-08-07 22:14:50 -0300

    Update install.sh
    add htop

--- install.sh
+++ install.sh
@@ -2,6 +2,8 @@
 
 sudo apt update
 
+sudo apt install htop
+
 
 ############################################# docker ###################################################
 ## https://www.digitalocean.com/community/tutorials/how-to-install-and-use-docker-on-ubuntu-20-04-pt ###
