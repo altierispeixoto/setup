088df1fa43d926936636187048cdc4b1c2d74fbc
Altiéris (Alt +R) <altierispeixoto@users.noreply.github.com>
2021-08-07 23:15:10 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,6 +8,18 @@ sudo apt install gnome-tweak-tool
 
 
 ############################################# docker ###################################################
+## https://www.edivaldobrito.com.br/como-instalar-o-google-chrome-no-ubuntu-20-04-via-repositorio-oficial/ ###
+########################################################################################################
+
+wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+
+sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
+
+sudo apt-get update
+
+sudo apt install google-chrome-stable
+
+############################################# docker ###################################################
 ## https://www.digitalocean.com/community/tutorials/how-to-install-and-use-docker-on-ubuntu-20-04-pt ###
 ########################################################################################################
 
