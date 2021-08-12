935e09f056fabf1617344fb7b73f6de17bb89487
Altiéris (Alt +R) <altierispeixoto@users.noreply.github.com>
2021-08-12 11:44:41 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -108,3 +108,8 @@ wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 chmod +x Miniconda3-latest-Linux-x86_64.sh
 ./Miniconda3-latest-Linux-x86_64.sh
 
+
+##################################
+## https://dbeaver.io/download/ ##
+##################################
+sudo snap install dbeaver-ce
